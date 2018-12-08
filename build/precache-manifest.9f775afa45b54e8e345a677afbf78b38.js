0bace4cca11a1f1ba7a88676144b044f1a77b539
Bambang Setiyawan <bse>
2018-12-08 17:30:25 +0700

    build test depploy

---
+++ build/precache-manifest.9f775afa45b54e8e345a677afbf78b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "775172a1603804741327",
+    "url": "/static/js/main.775172a1.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "775172a1603804741327",
+    "url": "/static/css/main.fb8275f8.chunk.css"
+  },
+  {
+    "revision": "e379c881e47ff2da99faf2e86b9a80ee",
+    "url": "/index.html"
+  }
+];
