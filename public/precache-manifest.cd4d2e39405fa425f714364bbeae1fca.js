a1c7c912dadf619a1d368794dfd448ec050addb9
Bambang Setiyawan <bse>
2018-12-08 19:50:42 +0700

    test build

---
+++ public/precache-manifest.cd4d2e39405fa425f714364bbeae1fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "503cf519f5c95446d402",
+    "url": "/static/js/main.503cf519.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "503cf519f5c95446d402",
+    "url": "/static/css/main.cfc5bd10.chunk.css"
+  },
+  {
+    "revision": "f436fcd9dde2e3fdfb718d5f6425d589",
+    "url": "/index.html"
+  }
+];
